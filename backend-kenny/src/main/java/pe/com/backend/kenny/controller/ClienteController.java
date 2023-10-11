497be358edc04bfd08bc63b05ecbb589b629f3c7
Nicolas AL <nicoal2204@gmail.com>
2023-10-10 21:28:31 -0500

    ClienteController Modificado (CRUD completo)

--- backend-kenny/src/main/java/pe/com/backend/kenny/controller/ClienteController.java
+++ backend-kenny/src/main/java/pe/com/backend/kenny/controller/ClienteController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,6 +34,16 @@ public class ClienteController {
     public Cliente registrarCliente(@RequestBody Cliente cliente)
 	{
 		return clienteService.registrarCliente(cliente);
+	}
+
+    @PutMapping("/actualizar")
+    public Cliente actualizarCliente(@RequestBody Cliente cliente){
+        return clienteService.actualizarCliente(cliente);
+    }
+
+    @DeleteMapping("/cliente/{idcli}")
+	public void eliminarCliente(@PathVariable int idcli) {
+		clienteService.eliminarCliente(idcli);
 	}
 
 
