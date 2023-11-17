80d7cadbf26d8a1872640d229b2b9ea1c3beb126
Enzo Jesús Esteban Quispe <eestebanq@odybank.com.pe>
2023-11-16 23:55:32 -0500

    feat: Path publico comidas id

--- backend-kenny/src/main/java/pe/com/backend/kenny/controller/publico/ComidaPublicoController.java
+++ backend-kenny/src/main/java/pe/com/backend/kenny/controller/publico/ComidaPublicoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,12 @@ public class ComidaPublicoController {
 	@GetMapping("/activos")
 	public Map<String, Object> listarComida(@RequestParam("pagina") Integer pagina){
 		return servicioComida.listarComidasCatalogo(pagina);
+	}
+	
+	@GetMapping("/{id}")
+	public Comida obtenerComida(@PathVariable String id)
+	{
+		return servicioComida.obtenerComida(id);
 	}
 	
 
