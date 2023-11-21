a0f82cea0f161377dabde9f016dba4c40e92a5ad
Enzo Jesús Esteban Quispe <eestebanq@odybank.com.pe>
2023-11-20 21:56:33 -0500

    feat: Correcion Postre

--- backend-kenny/src/main/java/pe/com/backend/kenny/controller/PostreController.java
+++ backend-kenny/src/main/java/pe/com/backend/kenny/controller/PostreController.java
@@ -4,21 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pe.com.backend.kenny.model.Postre;
 import pe.com.backend.kenny.model.TipoPostre;
+import pe.com.backend.kenny.repository.IPostreRepository;
 import pe.com.backend.kenny.service.ITipoPostreService;
 
 @RestController
-@RequestMapping("/postre")
+@RequestMapping("/intranet/postre")
 public class PostreController {
 	
 	@Autowired
 	private ITipoPostreService tipoPostreService;
 	
+	private @Autowired IPostreRepository postreRepository;
+	
 	@GetMapping("/tipos")
 	public List<TipoPostre> listarTiposPostre() {
 		return tipoPostreService.tiposPostre();
+	}
+	
+	@GetMapping("{id}")
+	public Postre obtenerPostrePorId(@PathVariable String id) {
+		return this.postreRepository.findByIdComida(id).get(0);
 	}
 }
